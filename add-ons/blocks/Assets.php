0fae9fafae709e478499db79f8783548d7848013
Thomas Gollenia <thomas@gollenia.at>
2021-12-09 17:02:48 +0100

    asstes renamed

--- add-ons/blocks/Assets.php
+++ add-ons/blocks/Assets.php
@@ -18,17 +18,17 @@ class Assets {
             $index_js     = '../../includes/backend.js';
             $script_asset = require( $script_asset_path );
             wp_register_script(
-                  'event-manager-blocks-block-editor',
+                  'events-block-editor',
                   plugins_url( $index_js, __FILE__ ),
                   $script_asset['dependencies'],
                   $script_asset['version']
             );
-            wp_set_script_translations( 'event-manager-blocks', 'em-pro', plugin_dir_path( __FILE__ ) . '../languages' );
+            wp_set_script_translations( 'events-block-editor', 'events', plugin_dir_path( __FILE__ ) . '../../languages' );
             
             $editor_css = '../../includes/backend.css';
 
             wp_register_style(
-                     'event-manager-blocks-block-editor',
+                     'events-block-style',
                      plugins_url( $editor_css, __FILE__ ),
                      array(),
                      filemtime( "$dir/$editor_css" )
@@ -43,8 +43,8 @@ class Assets {
 
         return [
             'style'         => 'event-manager-blocks-block-style',
-            'editor_script' => 'event-manager-blocks-block-editor',
-            'editor_style'  => 'event-manager-blocks-block-editor',
+            'editor_script' => 'events-block-editor',
+            'editor_style'  => 'events-block-style',
         ];
     
     }
