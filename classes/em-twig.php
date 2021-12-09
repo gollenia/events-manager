6e467f85921ccb47dd68e73afbc8868866aadc3a
Thomas Gollenia <thomas@gollenia.at>
2021-12-09 17:09:01 +0100

    some functions added

--- classes/em-twig.php
+++ classes/em-twig.php
@@ -2,13 +2,32 @@
 
 class EM_Twig {
     
-    public static function init($twig) {
-        $twig->addFunction( new \Twig\TwigFunction( 'option', [__CLASS__,'option'] ) );
-        return $twig;
+    private $twig;
+    public $locations = [];
+
+    public static function init() {
+        $instance = new self;
+        $instance->add_locations();
+
+        return $instance;
+        
+        //$instance->twig->addFunction( new \Twig\TwigFunction( 'option', [$instance,'option'] ) );
     }
 
-    public static function option($option) {
+    public function option($option) {
         return get_option($option);
+    }
+
+    private function add_locations() {
+        if(is_dir(get_theme_file_path() . "/plugins/events/")) {
+            array_push($this->locations, get_theme_file_path() . '/plugins/events/');
+        }
+    
+        if(is_dir(get_template_directory() . "/plugins/events/")) {
+            array_push($this->locations, get_template_directory() . 'events/');
+        }
+    
+        array_push($this->locations, EM_DIR . '/' . 'templates/');
     }
 }
 
