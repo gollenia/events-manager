e3034d0319f8fa2e378496a571633a07ed691164
Thomas Gollenia <thomas@gollenia.at>
2022-01-03 20:02:50 +0100

    booking build

--- includes/booking-form.asset.php
+++ includes/booking-form.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'ab9193ce28736169832f434d1a5eacc7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'a524e248b337d23f4badddddf52dfce1');
