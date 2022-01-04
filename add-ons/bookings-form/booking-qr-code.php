d7bda7ad54d109a7ff011c66ca4dfdd8f9f562fa
Thomas Gollenia <thomas@gollenia.at>
2022-01-04 14:29:46 +0100

    changed library

--- add-ons/bookings-form/booking-qr-code.php
+++ add-ons/bookings-form/booking-qr-code.php
@@ -4,6 +4,9 @@ use SepaQr\Data;
 use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelMedium;
 use Endroid\QrCode\Color\Color;
+use chillerlan\QRCode\{QRCode, QROptions};
+use chillerlan\QRCode\Data\QRMatrix;
+use chillerlan\QRCode\Common\EccLevel;
 
 /**
  * Register admin api that creates a banking qr code
@@ -19,24 +22,37 @@ class EM_QR_Code_Generator {
         
     }
 
-    public function generate_qr_code($request) {
+	/**
+	 * return an image containing the qr code
+	 *
+	 * @return void
+	 */
+    public function generate_qr_code() {
 		if(empty($_REQUEST['booking_id'])) return;
 		$booking = em_get_booking($_REQUEST['booking_id']);
-		 
+		$logo_id = get_option("em_offline_beneficiary", true);
+		$logo = wp_get_attachment_image( $logo_id );
 		$event = em_get_event($booking->event_id);
 		$paymentData = Data::create()
 			->setName(get_option("em_offline_beneficiary", true))
 			->setIban(get_option("em_offline_iban", true))
 			->setPurpose($_REQUEST['booking_id'] . "/" . $event->post_title . "/" . $booking->booking_meta['registration']['user_email'])
 			->setAmount($booking->booking_price);
-		$result = Builder::create()
-			->data($paymentData)
-			->errorCorrectionLevel(new ErrorCorrectionLevelMedium()) // required by EPC standard
-			->foregroundColor(new Color(255, 255, 255))
-			->backgroundColor(new Color(0, 0, 0, 127))
-			->build();
-		header('Content-Type: '.$result->getMimeType());
-		echo $result->getString();
+		$qrOptions = new QROptions([
+			'version' => 7,
+			'eccLevel' => QRCode::ECC_M, // required by EPC standard
+			'imageBase64' => false,
+			'addQuietzone'           => true,
+			'imageTransparent'       => false,
+			'keepAsSquare' => [QRMatrix::M_FINDER|QRMatrix::M_DARKMODULE, QRMatrix::M_LOGO, QRMatrix::M_FINDER_DOT, QRMatrix::M_ALIGNMENT|QRMatrix::M_DARKMODULE],
+			'drawCircularModules' => true,
+			'circleRadius' => 0.4,
+			'outputType' => QRCode::OUTPUT_MARKUP_SVG,
+			'svgConnectPaths' => true
+		]);
+		$result = new QRCode($qrOptions);
+		header('Content-Type: image/svg+xml');
+		echo $result->render($paymentData);
 		wp_die();
     }
 
