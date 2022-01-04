c06ad9686d180d3e8be7c84f263cd24b37e92115
Thomas Gollenia <thomas@gollenia.at>
2022-01-04 14:28:30 +0100

    Added SVG Support

--- add-ons/bookings-form/Components/OfflinePayment.jsx
+++ add-ons/bookings-form/Components/OfflinePayment.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { __ } from '@wordpress/i18n';
+import SVG, { Props as SVGProps } from 'react-inlinesvg';
 
 const OfflinePayment = (props) => {
 
@@ -28,16 +29,24 @@ const OfflinePayment = (props) => {
 	return (
 		<div>
 			<h4>{title}</h4>
-			<div className="grid md:grid--columns-3 xl:grid--columns-4">
-				<div><img src={`/wp-admin/admin-ajax.php?action=em_qr_code&booking_id=${bookingId}`}></img></div>
+			<div className="grid md:grid--columns-3 xl:grid--columns-4 grid--gap-12">
+				<div>
+					<div className="card card--no-image card--shadow bg-white card--center">
+						
+						<div className="card__content">
+							<div className="card__title">{__("Scan to pay", "em-pro")}</div>
+							<SVG className="w-full" src={`/wp-admin/admin-ajax.php?action=em_qr_code&booking_id=${bookingId}`}></SVG>
+						</div>
+					</div>
+				</div>
 				<div className='md:grid__column--span-2 xl:grid__column--span-3'>
 					<p dangerouslySetInnerHTML={{__html: html}}></p>
-					<table>
-						<tr><td>{__('Bank', 'em-pro')}</td><td>{paymentInfo.bank}</td></tr>
-						<tr><td>{__('IBAN', 'em-pro')}</td><td>{paymentInfo.iban}</td></tr>
-						<tr><td>{__('BIC', 'em-pro')}</td><td>{paymentInfo.bic}</td></tr>
-						<tr><td>{__('Beneficial', 'em-pro')}</td><td>{paymentInfo.beneficiary}</td></tr>
-						<tr><td>{__('Purpose', 'em-pro')}</td><td>{paymentInfo.purpose}</td></tr>
+					<table className="table--dotted">
+						<tr><th className='text-left'>{__('Bank', 'em-pro')}</th><td>{paymentInfo.bank}</td></tr>
+						<tr><th className='text-left'>{__('IBAN', 'em-pro')}</th><td>{paymentInfo.iban}</td></tr>
+						<tr><th className='text-left'>{__('BIC', 'em-pro')}</th><td>{paymentInfo.bic}</td></tr>
+						<tr><th className='text-left'>{__('Beneficial', 'em-pro')}</th><td>{paymentInfo.beneficiary}</td></tr>
+						<tr><th className='text-left'>{__('Purpose', 'em-pro')}</th><td>{paymentInfo.purpose}</td></tr>
 					</table>
 				</div>
 			</div>
