d351b8b50374b85aa2a8a56ba43f556ed65802c4
Thomas Gollenia <thomas@gollenia.at>
2022-01-14 13:30:08 +0100

    remittance from title no name

--- add-ons/bookings-form/booking-qr-code.php
+++ add-ons/bookings-form/booking-qr-code.php
@@ -38,7 +38,7 @@ class EM_QR_Code_Generator {
 		$paymentData = Data::create()
 			->setName(get_option("em_offline_beneficiary", true))
 			->setIban(get_option("em_offline_iban", true))
-			->setRemittanceText($_REQUEST['booking_id'] . "-" . $event->post_title . "-" . $booking->booking_meta['registration']['user_name'])
+			->setRemittanceText($_REQUEST['booking_id'] . "-" . $event->post_name . "-" . $booking->booking_meta['registration']['user_name'])
 			->setAmount($booking->booking_price);
 		$qrOptions = new QROptions([
 			'version' => 7,
@@ -71,7 +71,7 @@ class EM_QR_Code_Generator {
 		$event = em_get_event($booking->event_id);
 
 		$result = [
-			"purpose" => $_REQUEST['booking_id'] . "-" . $event->post_title . "-" . $booking->booking_meta['registration']['user_name'],
+			"purpose" => $_REQUEST['booking_id'] . "-" . $event->post_name . "-" . $booking->booking_meta['registration']['user_name'],
 			"iban" => get_option("em_offline_iban", true),
 			"beneficiary" => get_option("em_offline_beneficiary", true),
 			"bic" => get_option("em_offline_bic", true),
