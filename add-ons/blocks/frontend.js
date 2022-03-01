d567d64dc7b1228b1f66e64c84b8aaa4028dcbbd
Thomas Gollenia <thomas@gollenia.at>
2022-03-01 06:35:33 +0100

    new entry script for events frontend

---
+++ add-ons/blocks/frontend.js
@@ -0,0 +1,13 @@
+import Upcoming from './blocks/upcoming/frontend'
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener('DOMContentLoaded', () => {
+	const rootElements = document.getElementsByClassName('events-upcoming-block');
+	if(!rootElements) return
+	rootElements.forEach(element => {
+		ReactDOM.render( <Upcoming block={element.dataset.id}/>, element );
+	});
+	
+})
