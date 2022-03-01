cd391a69a7c4cf486a4b5c79323eba3e76e123da
Thomas Gollenia <thomas@gollenia.at>
2022-03-01 06:37:12 +0100

    rendering reduced to react hook

--- add-ons/blocks/Upcoming.php
+++ add-ons/blocks/Upcoming.php
@@ -7,83 +7,22 @@ class Upcoming extends Block {
         "upcoming"
     ];
 
+	/**
+	 * Undocumented function
+	 *
+	 * @param [type] $attributes
+	 * @param [type] $content
+	 * @param [type] $full_data
+	 * @return string
+	 */
     public function render($attributes, $content, $full_data) : string {
-        $attributes['events'] = $this->get_events($attributes);
-
-        $attributes['locations'] = $this->get_locations();
-		
-        $template = $this->get_template($full_data->name);
+        $block_id = uniqid();
+        $result = "<div class='events-upcoming-block' data-id='" . $block_id . "'></div>";
+		$result .= "<script>";
+		$result .= "if (typeof document.event_block_data === 'undefined') { document.event_block_data = {}; }";
+		$result .= "document.event_block_data['" . $block_id . "']=" . json_encode($attributes) . "</script>";
+		return $result;
         
-        return \Timber\Timber::compile($template, $attributes);
-        
-    }
-    
-    private function get_locations() {
-        
-        $args = [
-            'post_type' => 'location',
-            'post_status' => ['publish'],
-        ];
-        return \Timber\Timber::get_posts( $args );
-    }
-
-    private function get_events($attributes) {
-
-        $meta_query = [
-            [
-                'key' => '_event_start_date',
-                'value' => $attributes['fromDate'] ?: date('Y-m-d'),
-                'compare' => '>='
-            ]
-            ];
-
-        if($attributes['toDate']) {
-            array_push($meta_query, [
-                'key' => '_event_start_date',
-                'value' => $attributes['toDate'],
-                'compare' => '<'
-            ]);
-        }
-
-        if($attributes['selectedLocation']) {
-            array_push($meta_query, [
-                'key' => 'location_id',
-                'value' => $attributes['selectedLocation'],
-                'compare' => '=='
-            ]);
-        }
-
-        $tax_query = [];
-
-        if($attributes['selectedCategory']) {
-            array_push($tax_query, [
-                'taxonomy' => 'event-categories',
-                'terms' => $attributes['selectedCategory'],
-                'field' => 'id'
-            ]);
-        }
-
-        if($attributes['selectedTags']) {
-            array_push($tax_query, [
-                'taxonomy' => 'event-tags',
-                'terms' => $attributes['selectedTags'],
-                'field' => 'id'
-            ]);
-        }
-
-        $args = [
-            'post_type' => 'event',
-            'orderby' => '_event_start_date',
-            'order' => $attributes['order'],
-            'post_status' => ['publish'],
-            'meta_query' => $meta_query,
-            'tax_query' => !empty($tax_query) ? $tax_query : null, 
-            'posts_per_page' => $attributes['limit'],
-
-        ];
-
-        return \Timber\Timber::get_posts( $args );
-
     }
 
 }
