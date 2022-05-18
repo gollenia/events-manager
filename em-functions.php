e43df9dc467ff492448d59268e7f02479ffa39bd
Thomas Gollenia <thomas@gollenia.at>
2022-05-18 14:58:13 +0200

    moved and removed fns

--- em-functions.php
+++ em-functions.php
@@ -1,78 +1,5 @@
 <?php
 
-if(!function_exists('em_paginate')){ //overridable e.g. in you mu-plugins folder.
-/**
- * Takes a few params and determines a pagination link structure
- * @param string $link
- * @param int $total
- * @param int $limit
- * @param int $page
- * @param array $data If supplied and EM_USE_DATA_ATTS is true/defined, this set of data will be stripped from the URL and added as a data-em-ajax attribute containing data AJAX can use
- * @return string
- */
-function em_paginate($link, $total, $limit, $page=1, $data=array()){
-	if($limit > 0){
-		$pagesToShow = defined('EM_PAGES_TO_SHOW') ? EM_PAGES_TO_SHOW : 10;
-		$url_parts = explode('?', $link);
-		$base_link = $url_parts[0];
-		$base_querystring = '';
-		$data_atts = '';
-    	//Get querystring for first page without page
-    	if( count($url_parts) > 0 ){
-	    	$query_arr = array();
-	    	parse_str($url_parts[1], $query_arr);
-	    	//if $data was passed, strip any of these vars from both the $query_arr and $link for inclusion in the data-em-ajax attribute
-	    	if( !empty($data) && is_array($data) && (!defined('EM_USE_DATA_ATTS') || EM_USE_DATA_ATTS) ){
-	    		//remove the data attributes from $query_arr
-	    		foreach( array_keys($data) as $key){
-	    			if( array_key_exists($key, $query_arr) ){
-	    				unset($query_arr[$key]);
-	    			}
-	    		}
-	    		//rebuild the master link, without these data attributes
-	    		if( count($query_arr) > 0 ){
-	    			$link = $base_link .'?'. build_query($query_arr);
-	    		}else{
-	    			$link = $base_link;
-	    		}
-	    		$data_atts = 'data-em-ajax="'.esc_attr(build_query($data)).'"'; //for inclusion later on
-	    	}
-	    	//proceed to build the base querystring without pagination arguments
-	    	unset($query_arr['page']); unset($query_arr['pno']);
-	    	$base_querystring = esc_attr(build_query($query_arr));
-	    	if( !empty($base_querystring) ) $base_querystring = '?'.$base_querystring;
-    	}
-    	//calculate
-		$maxPages = ceil($total/$limit); //Total number of pages
-		$startPage = ($page <= $pagesToShow) ? 1 : $pagesToShow * (floor($page/$pagesToShow)) ; //Which page to start the pagination links from (in case we're on say page 12 and $pagesToShow is 10 pages)
-		$placeholder = urlencode('%PAGE%');
-		$link = str_replace('%PAGE%', $placeholder, esc_url($link)); //To avoid url encoded/non encoded placeholders
-	    //Add the back and first buttons
-		    $string = ($page>1 && $startPage != 1) ? '<a class="prev page-numbers" href="'.str_replace($placeholder,1,$link).'" title="1">&lt;&lt;</a> ' : '';
-		    if($page == 2){
-		    	$string .= ' <a class="prev page-numbers" href="'.esc_url($base_link.$base_querystring).'" title="2">&lt;</a> ';
-		    }elseif($page > 2){
-		    	$string .= ' <a class="prev page-numbers" href="'.str_replace($placeholder,$page-1,$link).'" title="'.($page-1).'">&lt;</a> ';
-		    }
-		//Loop each page and create a link or just a bold number if its the current page
-		    for ($i = $startPage ; $i < $startPage+$pagesToShow && $i <= $maxPages ; $i++){
-	            if($i == $page || (empty($page) && $startPage == $i)) {
-	                $string .= ' <strong><span class="page-numbers current">'.$i.'</span></strong>';
-	            }elseif($i=='1'){
-	                $string .= ' <a class="page-numbers" href="'.esc_url($base_link.$base_querystring).'" title="'.$i.'">'.$i.'</a> ';
-	            }else{
-	                $string .= ' <a class="page-numbers" href="'.str_replace($placeholder,$i,$link).'" title="'.$i.'">'.$i.'</a> ';
-	            }
-		    }
-		//Add the forward and last buttons
-		    $string .= ($page < $maxPages) ? ' <a class="next page-numbers" href="'.str_replace($placeholder,$page+1,$link).'" title="'.($page+1).'">&gt;</a> ' :' ' ;
-		    $string .= ($i-1 < $maxPages) ? ' <a class="next page-numbers" href="'.str_replace($placeholder,$maxPages,$link).'" title="'.$maxPages.'">&gt;&gt;</a> ' : ' ';
-		//Return the string
-		    return apply_filters('em_paginate', '<span class="em-pagination" '.$data_atts.'>'.$string.'</span>');
-	}
-}
-}
-
 /**
  * Creates a wp-admin style navigation.
  * @param string $link
@@ -165,7 +92,13 @@ function em_add_get_params($url, $params=array(), $html=true, $encode=true){
 function em_get_countries($add_blank = false, $sort = true){
 	global $em_countries_array;
 	if( !is_array($em_countries_array) ){
-		$em_countries_array = array ('AT' => __('Austria', 'events-manager'), 'DE' => __('Germany', 'events-manager'), 'CH' => __('Switzerland', 'events-manager'), 'FR' => __('France', 'events-manager'), 'NL' => __('Netherlands', 'events-manager') );
+		$em_countries_array = array (
+			'AT' => __('Austria', 'events-manager'), 
+			'DE' => __('Germany', 'events-manager'), 
+			'CH' => __('Switzerland', 'events-manager'), 
+			'FR' => __('France', 'events-manager'), 
+			'NL' => __('Netherlands', 'events-manager') 
+		);
 	}
 	if($sort){ asort($em_countries_array); }
 	if($add_blank !== false){
@@ -186,7 +119,7 @@ function em_get_scopes(){
 	$start_of_week = get_option('start_of_week');
 	$end_of_week_name = $start_of_week > 0 ? $wp_locale->get_weekday($start_of_week-1) : $wp_locale->get_weekday(6);
 	$start_of_week_name = $wp_locale->get_weekday($start_of_week);
-	$scopes = array(
+	return array(
 		'all' => __('All events','events-manager'),
 		'future' => __('Future events','events-manager'),
 		'past' => __('Past events','events-manager'),
@@ -203,43 +136,6 @@ function em_get_scopes(){
 		'6-months'  => __('Events within 6 months','events-manager'),
 		'12-months' => __('Events within 12 months','events-manager')
 	);
-	return apply_filters('em_get_scopes',$scopes);
-}
-
-function em_get_currencies(){
-	$currencies = new stdClass();
-	$currencies->names = array('EUR' => 'EUR - Euros','USD' => 'USD - U.S. Dollars','GBP' => 'GBP - British Pounds','CAD' => 'CAD - Canadian Dollars','AUD' => 'AUD - Australian Dollars','BRL' => 'BRL - Brazilian Reais','CZK' => 'CZK - Czech koruna','DKK' => 'DKK - Danish Kroner','HKD' => 'HKD - Hong Kong Dollars','HUF' => 'HUF - Hungarian Forints','ILS' => 'ILS - Israeli New Shekels','JPY' => 'JPY - Japanese Yen','MYR' => 'MYR - Malaysian Ringgit','MXN' => 'MXN - Mexican Pesos','TWD' => 'TWD - New Taiwan Dollars','NZD' => 'NZD - New Zealand Dollars','NOK' => 'NOK - Norwegian Kroner','PHP' => 'PHP - Philippine Pesos','PLN' => 'PLN - Polish Zlotys','SGD' => 'SGD - Singapore Dollars','SEK' => 'SEK - Swedish Kronor','CHF' => 'CHF - Swiss Francs','THB' => 'THB - Thai Baht','TRY' => 'TRY - Turkish Liras', 'RUB'=>'RUB - Russian Ruble');
-	$currencies->symbols = array( 'EUR' => '&euro;','USD' => '$','GBP' => '&pound;','CAD' => '$','AUD' => '$','BRL' => 'R$','CZK' => 'K&#269;','DKK' => 'kr','HKD' => '$','HUF' => 'Ft','JPY' => '&#165;','MYR' => 'RM','MXN' => '$','TWD' => '$','NZD' => '$','NOK' => 'kr','PHP' => 'Php', 'PLN' => '&#122;&#322;','SGD' => '$','SEK' => 'kr','CHF' => 'CHF','TRY' => 'TL','RUB'=>'&#8381;');
-	$currencies->true_symbols = array( 'EUR' => '€','USD' => '$','GBP' => '£','CAD' => '$','AUD' => '$','BRL' => 'R$','CZK' => 'Kč','DKK' => 'kr','HKD' => '$','HUF' => 'Ft','JPY' => '¥','MYR' => 'RM','MXN' => '$','TWD' => '$','NZD' => '$','NOK' => 'kr','PHP' => 'Php','PLN' => 'zł','SGD' => '$','SEK' => 'kr','CHF' => 'CHF','TRY' => 'TL', 'RUB'=>'₽');
-	return apply_filters('em_get_currencies',$currencies);
-}
-
-
-
-function em_get_currency_symbol($true_symbol = false, $currency = false){
-	$formatter = new \Contexis\Events\Intl\PriceFormatter(0);
-	return $formatter->get_currency();
-	
-}
-
-function em_get_currency_name($currency = false){
-	if( !$currency ) $currency = get_option('dbem_bookings_currency');
-	return apply_filters('em_get_currency_name', em_get_currencies()->names[$currency]);
-}
-
-function em_get_hour_format(){
-	$locale_code = substr ( get_locale(), 0, 2 );
-	if (preg_match('/^en_(?:GB|IE|AU|NZ|ZA|TT|JM)$/', get_locale())) {
-		$locale_code = 'en-GB';
-	}
-	//Set time
-	$show24Hours = ( !preg_match("/en|sk|zh|us|uk/", $locale_code ) );	// Setting 12 hours format for those countries using it
-	
-	return $show24Hours ? "H:i":"h:i A";
-}
-
-function em_get_days_names(){
-	return array (1 => __( 'Mon' ), 2 => __( 'Tue' ), 3 => __( 'Wed' ), 4 => __( 'Thu' ), 5 => __( 'Fri' ), 6 => __( 'Sat' ), 0 => __( 'Sun' ) );
 }
 
 /**
@@ -267,75 +163,6 @@ function em_wp_get_referer(){
 		return wp_get_referer();
 	}
 }
-
-/**
- * Copied straight from wp-login.php, only change atm is a function renaming.
- * Handles registering a new user.
- *
- * @param array associative array of user values to insert
- * @return int|WP_Error Either user's ID or error on failure.
- */
-/*function em_register_new_user( $user_data ) {
-	$user_data = apply_filters('em_register_new_user_pre',$user_data);
-	$errors = new WP_Error();
-	if( !empty($user_data['user_name']) ){
-		$name = explode(' ', $user_data['user_name']);
-		$user_data['first_name'] = array_shift($name);
-		$user_data['last_name'] = implode(' ',$name);
-	}
-	$sanitized_user_login = sanitize_user( $user_data['user_login'] );
-	$user_data['user_login'] = $sanitized_user_login;
-	$user_email = apply_filters( 'user_registration_email', $user_data['user_email'] );
-
-	// Check the username
-	if ( $sanitized_user_login == '' ) {
-		$errors->add( 'empty_username', __( '<strong>ERROR</strong>: Please enter a username.', 'events-manager') );
-	} elseif ( ! validate_username( $user_data['user_login'] ) ) {
-		$errors->add( 'invalid_username', __( '<strong>ERROR</strong>: This username is invalid because it uses illegal characters. Please enter a valid username.', 'events-manager') );
-		$sanitized_user_login = '';
-	} elseif ( username_exists( $sanitized_user_login ) ) {
-		$errors->add( 'username_exists', __( '<strong>ERROR</strong>: This username is already registered, please choose another one.', 'events-manager') );
-	}
-
-	// Check the e-mail address
-	if ( $user_email == '' ) {
-		$errors->add( 'empty_email', __( '<strong>ERROR</strong>: Please type your e-mail address.', 'events-manager') );
-	} elseif ( ! is_email( $user_email ) ) {
-		$errors->add( 'invalid_email', __( '<strong>ERROR</strong>: The email address isn&#8217;t correct.', 'events-manager') );
-		$user_email = '';
-	} elseif ( email_exists( $user_email ) ) {
-		$errors->add( 'email_exists', __( '<strong>ERROR</strong>: This email is already registered, please choose another one.', 'events-manager') );
-	}
-
-	do_action( 'register_post', $sanitized_user_login, $user_email, $errors );
-
-	//custom registration filter to prevent things like SI Captcha and other plugins of this kind interfering with EM
-	$errors = apply_filters( 'em_registration_errors', $errors, $sanitized_user_login, $user_email );
-	
-	if ( $errors->get_error_code() ) return $errors;
-
-	if(empty($user_data['user_pass'])){
-		$user_data['user_pass'] =  wp_generate_password( 12, false);
-	}
-
-	$user_id = wp_insert_user( $user_data );
-	if( is_numeric($user_id) && !empty($user_data['dbem_phone']) ){
-		update_user_meta($user_id, 'dbem_phone', $user_data['dbem_phone']);
-	}
-
-	if ( ! $user_id ) {
-		$errors->add( 'registerfail', sprintf( __( '<strong>ERROR</strong>: Couldn&#8217;t register you... please contact the <a href="mailto:%s">webmaster</a> !', 'events-manager'), get_option( 'admin_email' ) ) );
-		return $errors;
-	}
-
-	update_user_option( $user_id, 'default_password_nag', true, true ); //Set up the Password change nag.
-
-	global $em_temp_user_data;
-	$em_temp_user_data = $user_data; //for later useage
-	$em_temp_user_data['user_id'] = $user_id;
-
-	return apply_filters('em_register_new_user',$user_id);
-}*/
 
 /*
  * UI Helpers
