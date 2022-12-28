03f8a94717ab27ecc5f97d06a7b1af32e3df5841
Thomas Gollenia <thomas@gollenia.at>
2022-12-28 08:11:24 +0100

    new build

--- includes/backend.asset.php
+++ includes/backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'd05acf8cdf4ee3af9eb2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'ec92e52beb41ae2e251c');
--- includes/booking.asset.php
+++ includes/booking.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '03057c0eeccba79d0ab9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '5252cfcdf46a027de2a2');
