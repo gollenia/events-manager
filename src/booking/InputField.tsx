d8b303dfc3aa1cb98ee3386cdc4057b63a4b0785
Thomas Gollenia <thomas@gollenia.at>
2023-05-10 11:23:00 +0200

    tel -> phone

--- src/booking/InputField.tsx
+++ src/booking/InputField.tsx
@@ -39,7 +39,7 @@ const InputField: FC< InputFieldProps > = ( props ) => {
 				return <NumberInput { ...settings } onChange={ onChange } disabled={ disabled } />;
 			case 'textarea':
 				return <TextArea { ...settings } onChange={ onChange } disabled={ disabled } />;
-			case 'tel':
+			case 'phone':
 				return <Telephone { ...settings } onChange={ onChange } disabled={ disabled } />;
 			case 'checkbox':
 				return <Checkbox { ...settings } onChange={ onChange } disabled={ disabled } />;
