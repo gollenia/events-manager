8c9e9586ceaa79aa0a0917253a641c5d247d9eea
Thomas Gollenia <thomas@gollenia.at>
2024-01-15 13:13:34 +0100

    enable multiple booking buttons

--- src/booking.js
+++ src/booking.js
@@ -3,15 +3,17 @@ import Booking from './booking/index.js';
 import { publish } from './booking/modules/events.js';
 const rootElement = document.getElementById( 'booking_app' );
 
-const bookingButton = document.getElementById( 'booking_button' );
-const showBooking = new Event( 'showBooking' );
+const bookingButtons = document.getElementsByClassName( 'wp-block-events-manager-booking' );
+console.log( bookingButtons );
 
 if ( rootElement ) {
 	const root = createRoot( document.getElementById( 'booking_app' ) );
 
 	root.render( <Booking post={ rootElement.dataset.post } open={ false } />, root );
 
-	bookingButton.addEventListener( 'click', () => {
-		publish( 'showBooking', true );
-	} );
+	for ( let item of bookingButtons ) {
+		item.addEventListener( 'click', () => {
+			publish( 'showBooking', true );
+		} );
+	}
 }
