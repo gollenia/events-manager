58a74bd29ae6aa66c07d25e92d0e14d6480e4297
Thomas Gollenia <thomas@gollenia.at>
2025-01-15 10:20:30 +0000

    price removed

--- templates/emails/bookingtickets.php
+++ templates/emails/bookingtickets.php
@@ -6,7 +6,5 @@ echo $ticket_booking->get_ticket()->name;
 
 <?php _e('Quantity','events'); ?>: <?php echo $ticket_booking->get_spaces(); ?>
 
-<?php _e('Price','events'); ?>: <?php echo \Contexis\Events\Intl\Price::currency_symbol()." ". number_format($ticket_booking->get_price(),2); ?>
-
 
 <?php endforeach; ?>
