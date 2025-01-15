272705712c01968d53728b6c53eef87714c00593
Thomas Gollenia <thomas@gollenia.at>
2025-01-15 10:26:53 +0000

    price in

--- templates/emails/bookingtickets.php
+++ templates/emails/bookingtickets.php
@@ -6,5 +6,7 @@ echo $EM_Ticket_Booking->get_ticket()->name;
 
 <?php _e('Quantity','events-manager'); ?>: <?php echo $EM_Ticket_Booking->get_spaces(); ?>
 
+<?php _e('Price','events-manager'); ?>: <?php echo \Contexis\Events\Intl\Price::currency_symbol()." ". number_format($EM_Ticket_Booking->get_price(),2); ?>
+
 
 <?php endforeach; ?>
